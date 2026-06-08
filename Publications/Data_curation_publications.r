34fd078772a3b0523fff5d95927997fe7dd59e8e
violah-fellow <violah-fellow@gfi.org>
2026-06-08 16:03:24 +0200

    update data curation - reserach categories

--- Publications/Data_curation_publications.r
+++ Publications/Data_curation_publications.r
@@ -4,8 +4,8 @@ library(readxl)
 # Read in the search data (ALL entries)
 ## List with files
 file_list <- c(
-    list.files("Publications_Data/", pattern = "Search term*", full.names = TRUE),
-    list.files("Publications_Data/", pattern = "Top 500*", full.names = TRUE)
+    list.files("Publications/Publications_Data/", pattern = "Search term*", full.names = TRUE),
+    list.files("Publications/Publications_Data/", pattern = "Top 500*", full.names = TRUE)
 )
 
 ## read xlsx files
@@ -15,16 +15,17 @@ all_list <- lapply(file_list, read_excel, skip = 1)
 all <- all_list %>%
     bind_rows() %>%
     distinct(`Publication ID`, .keep_all = TRUE) %>%
-    dplyr::filter(`Document Type` == "Research Article")
+    dplyr::filter(`Publication Type` == "Article")
 
 # Read in in scope data
-in_scope <- read_excel("Publications_Data/IN SCOPE 2025.xlsx", skip = 1)
+in_scope <- read_excel("Publications/Publications_Data/IN SCOPE 2025.xlsx", skip = 1) %>%
+    dplyr::filter(`Publication Type` == "Article")
 
 # Read in labelled data
-labelled <- read_excel("Publications_Data/Copy of Publications data 2026.xlsx")
+labelled <- read_excel("Publications/Publications_Data/Copy of Publications data 2026.xlsx")
 
 ## filter for in scope
-labelled %>%
+labelled <- labelled %>%
     filter(`Publication ID` %in% in_scope$`Publication ID`)
 ## gives 1094 publications
 
@@ -60,94 +61,92 @@ all <- all %>%
     ) %>%
     dplyr::select(id, title, abstract, year, scope, pillar, research_category)
 
+# Repare/clean some research category labels
+all <- all %>%
+    mutate(research_category = str_remove(research_category, "\\ \\(incl Texturization methods\\)+")) %>%
+    mutate(research_category = ifelse(
+        research_category == "Consumer and market research", "Consumer & market research",
+        ifelse(research_category == "Health /nutrition", "Health & nutrition",
+            ifelse(research_category == "No sector assigned", "Other", research_category)
+        )
+    ))
+
 # write csv
-write_csv(all, "Publications_Data/publications_curated.csv")
+write_csv(all, "Publications/publications_curated.csv")
 
 # Is the labelling balanced?
 
 # AP pillar is somewhat balanced:
-# 1 CC       109
-# 2 CM       113
-# 3 F        165
-# 4 PB       716
-# 5 NA      4148
+# 1 CC        98
+# 2 CM        98
+# 3 F        151
+# 4 PB       695
+# 5 NA      3773
 
 # Research category is very unbalanced:
-#  1 Bioprocess design                              35
-#  2 Cell culture media                             10
-#  3 Cell line development                           9
-#  4 Consumer & market research                    134
-#  5 Consumer and market research                    1
-#  6 Crop development                               37
-#  7 End product formulation                       154
-#  8 Feedstocks                                     30
-#  9 Food safety & quality                          48
-# 10 Health & nutrition                             60
-# 11 Health /nutrition                               4
-# 12 Impact assessments                             39
-# 13 Ingredient optimisation                       357
-# 14 Manufacturing (incl Texturization methods)      2
-# 15 No sector assigned                              2
-# 16 Other                                          67
-# 17 Scaffolding                                    15
-# 18 Strain development                             29
-# 19 Target molecule selection                       5
-# 20 Texturization methods                          64
-# 21 NA                                           4149
+#  1 Bioprocess design             33
+#  2 Cell culture media             9
+#  3 Cell line development          8
+#  4 Consumer & market research   129
+#  5 Crop development              37
+#  6 End product formulation      148
+#  7 Feedstocks                    29
+#  8 Food safety & quality         45
+#  9 Health & nutrition            63
+# 10 Impact assessments            34
+# 11 Ingredient optimisation      344
+# 12 Other                         53
+# 13 Scaffolding                   14
+# 14 Strain development            27
+# 15 Target molecule selection      4
+# 16 Texturization methods         65
 
 # And by pillar
 # PB
-#  1 Consumer & market research                     65
-#  2 Crop development                               37
-#  3 End product formulation                       138
-#  4 Food safety & quality                          33
-#  5 Health & nutrition                             42
-#  6 Health /nutrition                               4
-#  7 Impact assessments                             19
-#  8 Ingredient optimisation                       293
-#  9 Manufacturing (incl Texturization methods)      1
-# 10 No sector assigned                              1
-# 11 Other                                          13
-# 12 Strain development                             12
-# 13 Texturization methods                          58
+#  1 Consumer & market research    64
+#  2 Crop development              37
+#  3 End product formulation      134
+#  4 Food safety & quality         33
+#  5 Health & nutrition            46
+#  6 Impact assessments            18
+#  7 Ingredient optimisation      283
+#  8 Other                         11
+#  9 Strain development            11
+# 10 Texturization methods         58
 
 # F
-#  1 Bioprocess design             24
-#  2 Consumer & market research     2
+#  1 Bioprocess design             23
+#  2 Consumer & market research     1
 #  3 End product formulation        5
-#  4 Feedstocks                    30
-#  5 Food safety & quality          4
+#  4 Feedstocks                    29
+#  5 Food safety & quality          2
 #  6 Health & nutrition            10
 #  7 Impact assessments             6
-#  8 Ingredient optimisation       46
-#  9 No sector assigned             1
-# 10 Other                         14
-# 11 Strain development            17
-# 12 Target molecule selection      5
-# 13 Texturization methods          1
+#  8 Ingredient optimisation       44
+#  9 Other                         10
+# 10 Strain development            16
+# 11 Target molecule selection      4
+# 12 Texturization methods          1
 
 # CM
-# 1 Bioprocess design             10
-#  2 Cell culture media            10
-#  3 Cell line development          9
-#  4 Consumer & market research    33
-#  5 End product formulation        3
-#  6 Food safety & quality          4
-#  7 Impact assessments             8
-#  8 Other                         17
-#  9 Scaffolding                   15
+#  1 Bioprocess design              9
+#  2 Cell culture media             9
+#  3 Cell line development          8
+#  4 Consumer & market research    32
+#  5 End product formulation        2
+#  6 Food safety & quality          3
+#  7 Impact assessments             5
+#  8 Other                         13
+#  9 Scaffolding                   14
 # 10 Texturization methods          3
-# 11 NA                             1
 
 # CC
-#  1 Bioprocess design                               1
-#  2 Consumer & market research                     34
-#  3 Consumer and market research                    1
-#  4 End product formulation                         8
-#  5 Food safety & quality                           7
-#  6 Health & nutrition                              8
-#  7 Impact assessments                              6
-#  8 Ingredient optimisation                        18
-#  9 Manufacturing (incl Texturization methods)      1
-# 10 Other                                          23
-# 11 Texturization methods                           2
+# 1 Bioprocess design              1
+# 2 Consumer & market research    32
+# 3 End product formulation        7
+# 4 Food safety & quality          7
+# 5 Health & nutrition             7
+# 6 Impact assessments             5
+# 7 Ingredient optimisation       17
+# 8 Other                         19
+# 9 Texturization methods          3
